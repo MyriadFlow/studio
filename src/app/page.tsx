3bfad03b6fde94a4ecf05630edaa9d36b80259b5
Adetayo <tuneymitch@gmail.com>
2024-06-24 16:44:11 +0100

    review page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,160 @@
+'use client'
+import { useState, useEffect } from 'react'
 import { Button, Navbar } from '@/components'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useWriteContract, useWalletClient, useChainId } from 'wagmi'
+import { abi } from '../lib/abi' // Adjust the path to your ABI file
+import { bytecode } from '../lib/bytecode'
+import { createPublicClient, http, Hex } from 'viem'
+import { baseSepolia } from 'viem/chains'
+import { useAccount } from 'wagmi'
+import Phygital from '../lib/phygital.json'
+import SimpleStorage from '../lib/SimpleStorage.json'
+import Simplestore from '../lib/Simplestore.json'
+import axios from 'axios'
+
+// const contractAddress = '0xcA1DE631D9Cb2e64C863BF50b83D18249dFb7054'
 
 export default function Home() {
+	const { address: walletAddress } = useAccount()
+
+	const [contractAddress, setContractAddress] = useState<
+		`0x${string}` | undefined
+	>()
+	const [error, setError] = useState<string | null>(null)
+	const [isDeployed, setIsDeployed] = useState(false)
+
+	const publicClient = createPublicClient({
+		chain: baseSepolia,
+		transport: http(),
+	})
+
+	const chainId = useChainId()
+	const { data: walletClient } = useWalletClient({ chainId })
+
+	async function verifyContract(
+		contractAddress: string,
+		contractSourceCode: string,
+		contractName: string,
+		compilerVersion: string,
+		constructorArguments: string
+	) {
+		const apiKey = 'RBNWT988E1EQS41KEP1P4GIHP279Y1TU7I'
+
+		const params = new URLSearchParams()
+		params.append('apikey', apiKey)
+		params.append('module', 'contract')
+		params.append('action', 'verifysourcecode')
+		params.append('contractaddress', contractAddress)
+		params.append('sourceCode', contractSourceCode)
+		params.append('codeformat', 'solidity-single-file')
+		params.append('contractname', contractName)
+		params.append('compilerversion', compilerVersion)
+		params.append('optimizationUsed', '0') // Change to '1' if optimization was used
+		params.append('runs', '200') // Change to the number of runs if optimization was used
+		params.append('constructorArguments', constructorArguments)
+
+		try {
+			const response = await axios.post(
+				'https://api-sepolia.etherscan.io/api',
+				params.toString()
+			)
+			console.log(apiKey)
+			console.log(contractAddress)
+			console.log(contractSourceCode)
+			console.log(contractName)
+			console.log(compilerVersion)
+			if (response.data.status === '1') {
+				console.log('Contract verified successfully')
+				console.log('Verification response:', response)
+				console.log('Verification Guid:', response.data.result)
+			} else {
+				console.log('Failed to verify contract:', response.data.result)
+			}
+		} catch (error) {
+			console.error('Error verifying contract:', error)
+		}
+	}
+	const deployContract = async () => {
+		if (!walletClient) {
+			throw new Error('Wallet client not available')
+		}
+
+		try {
+			const hash = await walletClient.deployContract({
+				abi: Simplestore.abi,
+				bytecode: Simplestore.bytecode as Hex,
+				account: walletAddress,
+			})
+
+			if (!hash) {
+				throw new Error('failed to execeute deploy contract transactiopn')
+			}
+
+			const txn = await publicClient.waitForTransactionReceipt({ hash })
+			setContractAddress(txn.contractAddress as `0x${string}`)
+			setIsDeployed(true)
+
+			return txn.contractAddress
+		} catch (error) {
+			console.error('Deployment error:', error)
+			setError('Error deploying contract: ' + error)
+		}
+	}
+
+	const handleDeploy = async (
+		event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		event.preventDefault()
+		try {
+			const address = await deployContract()
+			console.log('Contract deployed at:', address)
+		} catch (error) {
+			console.error('Error deploying contract:', error)
+			setError('Error deploying contract: ' + error)
+		}
+	}
+
+	const handleVerify = async (
+		event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		event.preventDefault()
+		if (!contractAddress) {
+			setError('No contract address found to verify.')
+			return
+		}
+		const contractSourceCode = `// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    // Private state variable to store a number
+    uint256 private number;
+
+    // Setter function to set the value of the number
+    function setNumber(uint256 _number) public {
+        number = _number;
+    }
+
+    // Getter function to get the value of the number
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+}`
+		try {
+			await verifyContract(
+				contractAddress as string,
+				contractSourceCode,
+				'SimpleStorage', // Contract name
+				'v0.8.0+commit.c7dfd78e', // Compiler version
+				''
+			)
+		} catch (error) {
+			setError('Error verifying contract: ' + error)
+			console.error('Error verifying contract:', error)
+		}
+	}
+
 	return (
 		<>
 			<Navbar />
@@ -35,6 +187,19 @@ export default function Home() {
 						</Button>
 					</Link>
 				</div>
+				<Button onClick={handleDeploy}>deploy Contract</Button>
+				{isDeployed && (
+					<button
+						className='p-2 px-6 bg-white text-black rounded-full mt-10 text-2xl'
+						onClick={handleVerify}
+					>
+						Verify
+					</button>
+				)}
+				{contractAddress && (
+					<p className='text-white mt-4'>Contract Address: {contractAddress}</p>
+				)}
+				{error && <p className='text-red-500 mt-4'>{error}</p>}
 				<Image
 					src='/images/blob-2.png'
 					alt='blob'
--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,171 +1,96 @@
+'use client'
 import { Button, Input, Label, Navbar, Textarea } from '@/components'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function Review() {
+	const [activeButton, setActiveButton] = useState('phygital')
+
+	const handleButtonClick = (buttonLabel: string) => {
+		setActiveButton(buttonLabel)
+	}
+
+	const isDevelopment = process.env.NODE_ENV === 'development'
+
+	const getData = () => {
+		if (typeof window !== 'undefined' && localStorage) {
+			return localStorage.getItem('phygitalData')
+		}
+	}
+
+	const storedData = getData() ?? '{}'
+	const parsedData = JSON.parse(storedData)
+
+	const apiUrl = isDevelopment
+		? 'http://localhost:3000' // Local development URL
+		: 'https://studio.myriadflow.com' // Production URL
+
+	const getPhygital = async () => {
+		const res = await fetch(`${apiUrl}/api/review/${'Phygital1'}`)
+
+		const result = await res.json()
+		console.log(result)
+	}
+
+	useEffect(() => {
+		getPhygital()
+	}, [])
+
 	return (
 		<>
 			<Navbar />
 			<main className='min-h-screen p-20 flex flex-col gap-12 text-black'>
 				<div className='flex justify-between items-center'>
-					<Button className='bg-[#4187D6] border border-black text-white'>
+					<Button
+						className={`${
+							activeButton === 'phygital'
+								? 'bg-[#4187D6] text-white'
+								: 'bg-transparent'
+						}  border border-black text-black`}
+						onClick={() => handleButtonClick('phygital')}
+					>
 						Phygital
 					</Button>
-					<Button className='bg-transparent border border-black text-black'>
+					<Button
+						className={`${
+							activeButton === 'phygital-details'
+								? 'bg-[#4187D6] text-white'
+								: 'bg-transparent'
+						}  border border-black text-black`}
+						onClick={() => handleButtonClick('phygital-details')}
+					>
 						Additional Details
 					</Button>
-					<Button className='bg-transparent border border-black text-black'>
+					<Button
+						className={`${
+							activeButton === 'webxr'
+								? 'bg-[#4187D6] text-white'
+								: 'bg-transparent'
+						}  border border-black text-black`}
+						onClick={() => handleButtonClick('webxr')}
+					>
 						WebXR Experience
 					</Button>
 				</div>
-				<div className='border border-black bg-white p-8'>
-					<div>
-						<h1 className='text-2xl '>Phygital</h1>
-						<p className='text-xl mt-8'>Chain: Base network</p>
-					</div>
-					<div className='flex gap-8'>
-						<div>
-							<div>
-								<Label className='text-xl mb-6'>Phygital name </Label>
-								<Input className='border-0 bg-[#0000001A] rounded' />
-							</div>
-							<div>
-								<Label className='text-xl mb-6'>Description</Label>
-								<Textarea className='border-0 bg-[#0000001A] rounded' />
-							</div>
-						</div>
-						<div className='border border-black p-8'>
-							<Image
-								src='/images/preview.png'
-								alt='preview'
-								height={200}
-								width={200}
-							/>
-						</div>
-					</div>
-					<div>
-						<h2>Category</h2>
-						<div className='bg-[#0000001A] rounded p-8 flex flex-wrap gap-12'>
-							<Button className='bg-white rounded-full text-black'>
-								Fashion
-							</Button>
-							<Button className='bg-white rounded-full text-black'>
-								Collectibles
-							</Button>
-							<Button className='bg-white rounded-full text-black'>
-								Art & Photography
-							</Button>
-							<Button className='bg-white rounded-full text-black'>
-								Functional items
-							</Button>
-							<Button className='bg-white rounded-full text-black'>
-								Luxury goods
-							</Button>
-							<Button className='bg-white rounded-full text-black'>
-								Sustainable materials
-							</Button>
-							<Button className='bg-white rounded-full text-black'>
-								Tech enabled
-							</Button>
-							<Button className='bg-white rounded-full text-black'>
-								Music lovers
-							</Button>
-						</div>
-					</div>
-					<div className='flex items-center justify-between'>
-						<div>
-							<Label className='text-xl mb-6'>Price*</Label>
-							<div className='flex gap-2'>
-								<Input className='border-0 bg-[#0000001A] rounded' />
-								<span>ETH</span>
-							</div>
-						</div>
-						<div>
-							<Label className='text-xl mb-6'>Quantity*</Label>
-							<div className='flex gap-2'>
-								<Input className='border-0 bg-[#0000001A] rounded' />
-								<span>items</span>
-							</div>
-						</div>
-						<div>
-							<Label className='text-xl mb-6'>Royalty</Label>
-							<div className='flex gap-2'>
-								<Input className='border-0 bg-[#0000001A] rounded' />
-								<span>%</span>
-							</div>
-						</div>
-					</div>
-					<div>
-						<h2 className='text-2xl'>Product Information for AI</h2>
-						<Textarea className='border-0 bg-[#0000001A] rounded' />
-					</div>
-					<div className='flex justify-between'>
-						<Button className='text-black bg-transparent rounded-full border border-black'>
-							Edit
-						</Button>
-						<Button className='text-black bg-[#30D8FF] rounded-full '>
-							Confirm
-						</Button>
-					</div>
-				</div>
 
-				<div className='border border-black bg-white p-8 '>
-					<h1 className='text-2xl'>Additional Details</h1>
-					<div className='flex flex-col gap-10 mt-8'>
+				{activeButton === 'phygital' && (
+					<div className='border border-black bg-white p-8'>
 						<div>
-							<Label className='text-xl mb-6'>Colour(s)  </Label>
-							<Input className='border-0 bg-[#0000001A] rounded w-full' />
+							<h1 className='text-2xl '>Phygital</h1>
+							<p className='text-xl mt-8'>Chain: Base network</p>
 						</div>
 						<div className='flex gap-8'>
-							<div className='basis-[70%]'>
-								<Label className='text-xl mb-6'>Size* </Label>
-								<Input className='border-0 bg-[#0000001A] rounded' />
-							</div>
-							<div className='basis-[30%]'>
-								<Label className='text-xl mb-6'>Weight*</Label>
-								<div className='flex gap-2'>
+							<div>
+								<div>
+									<Label className='text-xl mb-6'>Phygital name </Label>
 									<Input className='border-0 bg-[#0000001A] rounded' />
-									<span>Kg</span>
+								</div>
+								<div>
+									<Label className='text-xl mb-6'>Description</Label>
+									<Textarea className='border-0 bg-[#0000001A] rounded' />
 								</div>
 							</div>
-						</div>
-						<div>
-							<Label className='text-xl mb-6'>Usage</Label>
-							<Input className='border-0 bg-[#0000001A] rounded w-2/5' />
-						</div>
-						<div>
-							<Label className='text-xl mb-6'>Material</Label>
-							<Input className='border-0 bg-[#0000001A] rounded w-2/5' />
-						</div>
-						<div>
-							<Label className='text-xl mb-6'>Unique qualities</Label>
-							<Textarea className='border-0 bg-[#0000001A] rounded' />
-						</div>
-						<div className='flex justify-between '>
-							<div>
-								<Label className='text-xl mb-6'>Manufacturer </Label>
-								<Input className='border-0 bg-[#0000001A] rounded w-full' />
-							</div>
-							<div>
-								<Label className='text-xl mb-6'>Made  in</Label>
-								<Input className='border-0 bg-[#0000001A] rounded w-full' />
-							</div>
-						</div>
-						<div className='flex justify-between'>
-							<Button className='text-black bg-transparent rounded-full border border-black'>
-								Edit
-							</Button>
-							<Button className='text-black bg-[#30D8FF] rounded-full '>
-								Confirm
-							</Button>
-						</div>
-					</div>
-				</div>
-				<div className='border border-black bg-white p-8'>
-					<h1 className='text-2xl'>WebXR experience</h1>
-					<div className='flex flex-col gap-10 mt-8'>
-						<div className='flex gap-8'>
-							<div className='flex flex-col gap-12'>
-								<h2 className='text-xl'>Your Avatar</h2>
+							<div className='border border-black p-8'>
 								<Image
 									src='/images/preview.png'
 									alt='preview'
@@ -173,85 +98,62 @@ export default function Review() {
 									width={200}
 								/>
 							</div>
-							<div className='flex flex-col gap-12 w-full'>
-								<h2 className='text-xl'>Your WebXR Background</h2>
-								<div className='h-60 w-full flex flex-col items-center justify-center bg-[#D9D8D880] border border-black'>
-									<p>360 Image</p>
-								</div>
-							</div>
 						</div>
 						<div>
-							<h2 className='text-xl capitalize'>
-								available customization options for the avatars
-							</h2>
+							<h2>Category</h2>
 							<div className='bg-[#0000001A] rounded p-8 flex flex-wrap gap-12'>
 								<Button className='bg-white rounded-full text-black'>
-									Gender
+									Fashion
 								</Button>
 								<Button className='bg-white rounded-full text-black'>
-									Face
+									Collectibles
 								</Button>
 								<Button className='bg-white rounded-full text-black'>
-									Skin Color
+									Art & Photography
 								</Button>
 								<Button className='bg-white rounded-full text-black'>
-									Hair
+									Functional items
 								</Button>
 								<Button className='bg-white rounded-full text-black'>
-									Clothing
+									Luxury goods
 								</Button>
 								<Button className='bg-white rounded-full text-black'>
-									Accessories
+									Sustainable materials
 								</Button>
 								<Button className='bg-white rounded-full text-black'>
-									Animations
+									Tech enabled
+								</Button>
+								<Button className='bg-white rounded-full text-black'>
+									Music lovers
 								</Button>
 							</div>
 						</div>
-						<div>
-							<h2 className='text-xl'>
-								Free NFTs given users who interact with your avatar
-							</h2>
-							<div className='flex justify-between'>
-								<h2 className='text-xl'>Free NFT Image</h2>
-								<div className='border border-black p-8'>
-									<Image
-										src='/images/preview.png'
-										alt='preview'
-										height={200}
-										width={200}
-									/>
+						<div className='flex items-center justify-between'>
+							<div>
+								<Label className='text-xl mb-6'>Price*</Label>
+								<div className='flex gap-2'>
+									<Input className='border-0 bg-[#0000001A] rounded' />
+									<span>ETH</span>
+								</div>
+							</div>
+							<div>
+								<Label className='text-xl mb-6'>Quantity*</Label>
+								<div className='flex gap-2'>
+									<Input className='border-0 bg-[#0000001A] rounded' />
+									<span>items</span>
+								</div>
+							</div>
+							<div>
+								<Label className='text-xl mb-6'>Royalty</Label>
+								<div className='flex gap-2'>
+									<Input className='border-0 bg-[#0000001A] rounded' />
+									<span>%</span>
 								</div>
 							</div>
 						</div>
 						<div>
-							<p>
-								Weekly profit rewards given for the NFT owner and supporters if
-								your avatar reaches the avatar leaderboard top 3
-							</p>
-							<div className='flex items-center justify-between'>
-								<div>
-									<Label className='text-xl mb-6'>Gold</Label>
-									<div className='flex gap-2'>
-										<Input className='border-0 bg-[#0000001A] rounded' />
-										<span>%</span>
-									</div>
-								</div>
-								<div>
-									<Label className='text-xl mb-6'>Silver</Label>
-									<div className='flex gap-2'>
-										<Input className='border-0 bg-[#0000001A] rounded' />
-										<span>%</span>
-									</div>
-								</div>
-								<div>
-									<Label className='text-xl mb-6'>Bronze</Label>
-									<div className='flex gap-2'>
-										<Input className='border-0 bg-[#0000001A] rounded' />
-										<span>%</span>
-									</div>
-								</div>
-							</div>
+							<h2 className='text-2xl'>Product Information for AI</h2>
+							<Textarea className='border-0 bg-[#0000001A] rounded' />
 						</div>
 						<div className='flex justify-between'>
 							<Button className='text-black bg-transparent rounded-full border border-black'>
@@ -262,7 +164,168 @@ export default function Review() {
 							</Button>
 						</div>
 					</div>
-				</div>
+				)}
+
+				{activeButton === 'phygital-details' && (
+					<div className='border border-black bg-white p-8 '>
+						<h1 className='text-2xl'>Additional Details</h1>
+						<div className='flex flex-col gap-10 mt-8'>
+							<div>
+								<Label className='text-xl mb-6'>Colour(s)  </Label>
+								<Input className='border-0 bg-[#0000001A] rounded w-full' />
+							</div>
+							<div className='flex gap-8'>
+								<div className='basis-[70%]'>
+									<Label className='text-xl mb-6'>Size* </Label>
+									<Input className='border-0 bg-[#0000001A] rounded' />
+								</div>
+								<div className='basis-[30%]'>
+									<Label className='text-xl mb-6'>Weight*</Label>
+									<div className='flex gap-2'>
+										<Input className='border-0 bg-[#0000001A] rounded' />
+										<span>Kg</span>
+									</div>
+								</div>
+							</div>
+							<div>
+								<Label className='text-xl mb-6'>Usage</Label>
+								<Input className='border-0 bg-[#0000001A] rounded w-2/5' />
+							</div>
+							<div>
+								<Label className='text-xl mb-6'>Material</Label>
+								<Input className='border-0 bg-[#0000001A] rounded w-2/5' />
+							</div>
+							<div>
+								<Label className='text-xl mb-6'>Unique qualities</Label>
+								<Textarea className='border-0 bg-[#0000001A] rounded' />
+							</div>
+							<div className='flex justify-between '>
+								<div>
+									<Label className='text-xl mb-6'>Manufacturer </Label>
+									<Input className='border-0 bg-[#0000001A] rounded w-full' />
+								</div>
+								<div>
+									<Label className='text-xl mb-6'>Made  in</Label>
+									<Input className='border-0 bg-[#0000001A] rounded w-full' />
+								</div>
+							</div>
+							<div className='flex justify-between'>
+								<Button className='text-black bg-transparent rounded-full border border-black'>
+									Edit
+								</Button>
+								<Button className='text-black bg-[#30D8FF] rounded-full '>
+									Confirm
+								</Button>
+							</div>
+						</div>
+					</div>
+				)}
+
+				{activeButton === 'webxr' && (
+					<div className='border border-black bg-white p-8'>
+						<h1 className='text-2xl'>WebXR experience</h1>
+						<div className='flex flex-col gap-10 mt-8'>
+							<div className='flex gap-8'>
+								<div className='flex flex-col gap-12'>
+									<h2 className='text-xl'>Your Avatar</h2>
+									<Image
+										src='/images/preview.png'
+										alt='preview'
+										height={200}
+										width={200}
+									/>
+								</div>
+								<div className='flex flex-col gap-12 w-full'>
+									<h2 className='text-xl'>Your WebXR Background</h2>
+									<div className='h-60 w-full flex flex-col items-center justify-center bg-[#D9D8D880] border border-black'>
+										<p>360 Image</p>
+									</div>
+								</div>
+							</div>
+							<div>
+								<h2 className='text-xl capitalize'>
+									available customization options for the avatars
+								</h2>
+								<div className='bg-[#0000001A] rounded p-8 flex flex-wrap gap-12'>
+									<Button className='bg-white rounded-full text-black'>
+										Gender
+									</Button>
+									<Button className='bg-white rounded-full text-black'>
+										Face
+									</Button>
+									<Button className='bg-white rounded-full text-black'>
+										Skin Color
+									</Button>
+									<Button className='bg-white rounded-full text-black'>
+										Hair
+									</Button>
+									<Button className='bg-white rounded-full text-black'>
+										Clothing
+									</Button>
+									<Button className='bg-white rounded-full text-black'>
+										Accessories
+									</Button>
+									<Button className='bg-white rounded-full text-black'>
+										Animations
+									</Button>
+								</div>
+							</div>
+							<div>
+								<h2 className='text-xl'>
+									Free NFTs given users who interact with your avatar
+								</h2>
+								<div className='flex justify-between'>
+									<h2 className='text-xl'>Free NFT Image</h2>
+									<div className='border border-black p-8'>
+										<Image
+											src='/images/preview.png'
+											alt='preview'
+											height={200}
+											width={200}
+										/>
+									</div>
+								</div>
+							</div>
+							<div>
+								<p>
+									Weekly profit rewards given for the NFT owner and supporters
+									if your avatar reaches the avatar leaderboard top 3
+								</p>
+								<div className='flex items-center justify-between'>
+									<div>
+										<Label className='text-xl mb-6'>Gold</Label>
+										<div className='flex gap-2'>
+											<Input className='border-0 bg-[#0000001A] rounded' />
+											<span>%</span>
+										</div>
+									</div>
+									<div>
+										<Label className='text-xl mb-6'>Silver</Label>
+										<div className='flex gap-2'>
+											<Input className='border-0 bg-[#0000001A] rounded' />
+											<span>%</span>
+										</div>
+									</div>
+									<div>
+										<Label className='text-xl mb-6'>Bronze</Label>
+										<div className='flex gap-2'>
+											<Input className='border-0 bg-[#0000001A] rounded' />
+											<span>%</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className='flex justify-between'>
+								<Button className='text-black bg-transparent rounded-full border border-black'>
+									Edit
+								</Button>
+								<Button className='text-black bg-[#30D8FF] rounded-full '>
+									Confirm
+								</Button>
+							</div>
+						</div>
+					</div>
+				)}
 			</main>
 		</>
 	)
