b598739e4cbcbaca9ce8ebed095d83430268a2d2
Suraj Mahto <surajmahto255@gmail.com>
2024-08-06 16:11:04 +0530

    feat : updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['utfs.io'],
+	  domains: ['utfs.io'],
 	},
 	experimental: {
-		missingSuspenseWithCSRBailout: false,
+	  missingSuspenseWithCSRBailout: false,
 	},
-}
-
-export default nextConfig
+	async headers() {
+	  return [
+		{
+		  source: '/(.*)', // Apply this to all routes
+		  headers: [
+			{
+			  key: 'Content-Security-Policy',
+			  value: "frame-ancestors 'self' http://localhost:* https://*.vercel.app https://*.ngrok-free.app https://secure-mobile.walletconnect.com https://secure.walletconnect.com",
+			},
+		  ],
+		},
+	  ]
+	},
+  }
+  
+  export default nextConfig
+  
