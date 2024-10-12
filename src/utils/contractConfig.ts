bcc128d5d88060826909506803023c3196381e91
Suraj Mahto <surajmahto255@gmail.com>
2024-10-12 22:36:14 +0530

    feat : comment contractconfig.ts

--- src/utils/contractConfig.ts
+++ src/utils/contractConfig.ts
@@ -1,9 +1,9 @@
-import { http, createConfig } from '@wagmi/core'
-import { base } from 'viem/chains'
+// import { http, createConfig } from '@wagmi/core'
+// import { base } from 'viem/chains'
 
-export const config = createConfig({
-	chains: [base],
-	transports: {
-		[base.id]: http(),
-	},
-})
+// export const config = createConfig({
+// 	chains: [base],
+// 	transports: {
+// 		[base.id]: http(),
+// 	},
+// })
